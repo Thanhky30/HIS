635aae369d01224611f30d63f4e1494fb3d4664e
Thanh Ky <thanhky777@gmail.com>
2026-08-09 21:43:22 +0700

    test controller

--- HIS/Controllers/WeatherForecastController.cs
+++ HIS/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace HIS.Controllers
     {
         private static readonly string[] Summaries =
         [
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chillyư423", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         ];
 
         [HttpGet(Name = "GetWeatherForecast")]
